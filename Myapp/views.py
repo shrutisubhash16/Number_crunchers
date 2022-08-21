c2faee121e9c6603d8b403ccf115db43f71853b5
shrutisubhash16 <68190970+shrutisubhash16@users.noreply.github.com>
2022-08-21 20:30:48 +0530

    addition

--- Myapp/views.py
+++ Myapp/views.py
@@ -55,7 +55,8 @@ def postsignIn_admin(request):
     except:
         message="invalid credentials"
         return render(request,"admin_signIn.html",{"messg":message})
-    return render(request,"admin_signIn.html")
+    message="invalid credentials"
+    return render(request,"admin_signIn.html",{"messg":message})
     #print(user['idToken'])
     
  
@@ -77,44 +78,45 @@ def postsignIn(request):
     return render(request, "wel.html",{"e":email})
  
 
-from difflib import SequenceMatcher
 def comparison(request):
-    num_plate = database.child('number_plate_data').get()
-    rfid_data = database.child('rfid_data').get()
-    rfid_dect = database.child('rfid_detection_data').get()
-
-    num = []
-    rfid_d = []
-    rfid_dt = []
-
-    for user in num_plate.each():
-        num.append(user.val())
-        #print(num)
+    num_perc = database.child('number_plate_percentage').get()
+    num_data = database.child('number_plate_data').get()
+    last_Data = database.child('last_detected_RFID').get()
     
-    for user in rfid_data.each():
-        rfid_d.append(user.val())
-        #print(rfid_d)
-    
-    for user in rfid_dect.each():
-        rfid_dt.append(user.val())
-        #print(rfid_dt)
-    
-    rfid_val = rfid_dt[-1]['rfid']
-    match_num = rfid_d[0][rfid_val]
-    extract_num = num[-1]['number_plate']
-    #extract_percent = num[-1]['percentage']
 
-    per = SequenceMatcher(None, match_num, extract_num).ratio() 
-    perc = "{:.2f}".format(per*100)
+    #print(original)
 
-    context = {
-        "rfid_val": rfid_val,
-        "match_num": match_num,
-        "extract_num":extract_num,
-        "extract_percent":perc
+    li = []
+    data = []
+    last_rfid = []
+
+    for i in num_perc.each():
+        li.append(i.val())
+
+    for i in num_data.each():
+        data.append(i.val())
+
+    for i in last_Data.each():
+        last_rfid.append(i.val())
+
+
+    
+    
+    perc = round(li[-1],2)
+    extract = data[-1]['Number_Plate']
+    rfid = last_rfid[-1]
+
+    original = database.child('rfid_data').child("-N9v0HDas-iTXT-9BSyK").child(rfid).get().val()
+    
+
+
+    context={
+        "perc": perc,
+        "extract":extract,
+        "original": original
+
     }
-
-    return render(request,'compare.html',context)
+    return render(request,"compare.html",context)
 
 
 def logout(request):
@@ -138,8 +140,9 @@ def postsignUp(request):
         #database.child(“users“).child(uid).child(“details“).set(data)
     except:
         message="Unable to create account try again"
-        return render(request,"signUp.html",{"message":message})
-    return render(request,"signIn.html")
+        return render(request,"signUp.html",{"msg":message})
+    m = "User added successfully"
+    return render(request,"signUp.html",{"msg":m})
 
 def reset(request):
     return render(request, "Reset.html")
@@ -148,11 +151,30 @@ def postReset(request):
     email = request.POST.get('email')
     try:
         authe.send_password_reset_email(email)
-        message  = "A email to reset password is successfully sent"
+        message  = "A email to reset password is successfully sent. Please check you spam folder"
         return render(request, "Reset.html", {"msg":message})
     except:
         message  = "Something went wrong, Please check the email you provided is registered or not"
         return render(request, "Reset.html", {"msg":message})
+
+def load(request):
+    return render(request, "load.html")
+
+from django.http import JsonResponse
+def getUsers(request):
+    data = database.child('Weights').get()
+    weight = []
+    time = []
+
+    for i in data.each():
+        weight.append(i.val())
+        time.append(i.key())
+
+    context={
+        "weight":weight[-1],
+        "time" : time[-1]
+    }
+    return JsonResponse({"users":context})
 
 
 class ChartData(APIView):
--- Number_crunchers/urls.py
+++ Number_crunchers/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from unicodedata import name
 from django.contrib import admin
 from django.urls import path
 from Myapp import views
@@ -30,4 +31,6 @@ urlpatterns = [
     path('reset/',views.reset, name="reset"),
     path('postReset/',views.postReset),
     path('compare/',views.comparison, name="compare"),
+    path('load/',views.load, name="load"),
+    path('ajax/getUsers',views.getUsers,name="getUsers")
 ]
